e7ac9585462e79a6ab9003808928710fb6d09b3a
Sergey Efimov <sergeyefimov234@gmail.com>
2022-02-09 13:12:25 +0300

    D Commit

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,9 @@
 public class BonusMilesService {
-    public int calculate(int coast) {
-        return coast;
+    public int calculate(int cost) {
+        int price = 10_000;
+        int miles = price / 20;
+        return miles;
+
+
     }
 }
